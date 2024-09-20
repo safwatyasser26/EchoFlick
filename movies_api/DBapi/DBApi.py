665e018f974f380fe804e7efeb2c816cab4a9505
YousefMohamedSalah <65715143+YousefMohammad@users.noreply.github.com>
2024-09-20 03:22:11 +0300

    Delete movies_api directory

--- movies_api/DBapi/DBApi.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from movie_route import movie
-
-app = FastAPI()
-app.include_router(movie)
-
-
--- movies_api/recommendation_api/DBApi.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from movie import recommender
-
-app = FastAPI()
-app.include_router(recommender)
-
-
